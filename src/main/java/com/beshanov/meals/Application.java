b758a211abda1349213929fb8ff1418a2d005c26
beshanov <beshanov@speechpro.com>
2022-11-08 00:38:56 +0300

    spring boot, web, test dependencies

--- src/main/java/com/beshanov/meals/Application.java
+++ src/main/java/com/beshanov/meals/Application.java
@@ -1,7 +1,18 @@
 package com.beshanov.meals;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
 public class Application {
 
+  public static void main(String[] args) {
+    new SpringApplicationBuilder()
+        .bannerMode(Banner.Mode.OFF)
+        .sources(Application.class).run(args);
+  }
 
 
 }
